05fd6052ad7d0a13f7b3fd980dabe23b1352cb24
bmcorser <bmcorser@gmail.com>
2016-11-15 15:02:39 +0000

    Starting login code

--- browser/test.js
+++ browser/test.js
@@ -1,23 +1,48 @@
-casper.test.begin('Google search retrieves 10 or more results', 5, function suite(test) {
-    casper.start("http://www.google.fr/", function() {
-        test.assertTitle("Google", "google homepage title is the one expected");
-        test.assertExists('form[action="/search"]', "main form is found");
-        this.fill('form[action="/search"]', {
-            q: "casperjs"
+var env = require('system').env;
+casper.options.viewportSize = {
+  width: 1024,
+  height: 768,
+};
+var login = function (casper) {
+  casper.thenOpen(env.CDMS_BASE_URL)
+  casper.waitForSelector(
+    '#ctl00_ContentPlaceHolder1_PassiveIdentityProvidersDropDownList',
+    function () {
+      this.fill('form#aspnetForm', {
+          ctl00$ContentPlaceHolder1$PassiveIdentityProvidersDropDownList: env.CDMS_ADFS_URL,
         }, true);
-    });
+    }
+  );
+  casper.then(function () {
+    this.click('#ctl00_ContentPlaceHolder1_PassiveSignInButton');
+  });
+  casper.waitForSelector('#ContentPlaceHolder1_SubmitButton',
+    function () {
+      this.sendKeys('#ContentPlaceHolder1_UsernameTextBox', env.CDMS_USERNAME);
+      this.sendKeys('#ContentPlaceHolder1_PasswordTextBox', env.CDMS_PASSWORD);
+      this.click('#ContentPlaceHolder1_SubmitButton');
+    }
+  ).then(function () {
+    if (this.getTitle() == 'Mobile Express - Microsoft Dynamics CRM') {
+      casper.echo('Logged in');
+      this.thenOpen(env.CDMS_BASE_URL);
+    } else {
+      casper.echo('Waiting for login to succeed');
+      login(casper);
+    }
+  });
+};
 
-    casper.wait(5000);
-
-    casper.then(function() {
-        test.assertTitle("casperjs - Recherche Google", "google title is ok");
-        test.assertUrlMatch(/q=casperjs/, "search term has been submitted");
-        test.assertEval(function() {
-            return __utils__.findAll("h3.r").length >= 10;
-        }, "google search for \"casperjs\" retrieves 10 or more results");
-    });
-
-    casper.run(function() {
-        test.done();
-    });
+casper.test.begin('Login to CDMS', 1, function suite(test) {
+  casper.start(env.CDMS_BASE_URL);
+  login(casper);
+  casper.waitForSelector('#CrmChart', function () {
+    test.assert(true);
+  });
+  /*
+  */
+  casper.wait(500000);
+  casper.run(function () {
+      test.done();
+  });
 });
